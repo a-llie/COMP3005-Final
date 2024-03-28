c46078f32f36f3dbaedb876d9831bbdcde9af9fb
Liam Stickle <lagstickle@gmail.com>
2024-03-27 23:12:51 -0400

    function signatures
    added some basic files and signatures for expected functions alone with some comments for the more complex

---
+++ Admin.py
@@ -0,0 +1,38 @@
+
+
+class Admin():
+
+    def get_trainer_schedual(id):
+        pass
+
+    def get_class_schedual():
+        pass
+
+    def add_class(exercise, capacity = 5):
+        #remove that block from the free scedual of the trainer
+        #pick a free room (in other.py)
+        #use the creat class func(in other.py)
+
+        pass
+
+    def remove_class():
+        pass
+
+    def get_rooms():
+        pass
+
+    def get_all_equipment():
+        pass
+
+    def maintain_equipment(id):
+        #simulates maintaining the equipment
+        pass
+
+    def get_member():
+        pass
+
+    def bill_all():
+        pass
+
+    def bill_member(username):
+        pass
